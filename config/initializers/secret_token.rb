4b105ce168763baa19117192c1fa6dee68642232
Maxim Kremenev <ezotrank@gmail.com>
2012-09-18 18:56:25 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NapolnenieWeb::Application.config.secret_token = '9915d788f1cf70f74c1f68e8f18480786dd10828814848e388cdabd4b2cda3aa7ccfc2dc8c0a04c3a389693719e515fd4cf49ba0bd7b3941c03d76f320ac680c'
