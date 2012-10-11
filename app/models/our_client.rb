447965857e94eec108c09e0b5aa46e89d0b5d5c9
Maxim Kremenev <ezotrank@gmail.com>
2012-10-11 17:31:06 +0300

    fix slider photo

--- app/models/our_client.rb
+++ app/models/our_client.rb
@@ -1,7 +1,7 @@
 class OurClient < ActiveRecord::Base
   attr_accessible :logo, :title
 
-  has_attached_file :logo, styles: { medium: "300x300", thumb: "125x125" },
+  has_attached_file :logo, styles: { thumb: "110x110!" },
                              path: ":rails_root/public/system/our_client/:attachment/:id/:style/:filename",
                              url: "/system/our_client/:attachment/:id/:style/:filename",
                              default_style: :thumb, default_url: 'loading.gif'
