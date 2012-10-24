c13ff536f3b42f42363e41e5878cfa1f97494edc
Maxim Kremenev <ezotrank@gmail.com>
2012-10-24 13:09:17 +0300

    fix title

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,6 @@ class StaticPagesController < ApplicationController
   def show
     @page = StaticPage.find_by_permalink(params[:permalink])
     redirect_to root_path and return unless @page.present?
+    set_seo_by_page(@page)
   end
 end
